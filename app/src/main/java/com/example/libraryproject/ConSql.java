fd5470f0fca30c8868a3b6859732081b439de472
irthayagraj <gayathri.rp03@gmail.com>
2022-12-03 22:05:03 +0530

    Initial Setup

---
+++ app/src/main/java/com/example/libraryproject/ConSql.java
@@ -0,0 +1,29 @@
+package com.example.libraryproject;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConSql {
+    Connection con;
+    @SuppressLint("NewApi")
+    public Connection conClass(){
+        String ip="192.168.127.15",port="1433",db="librarymanagement",username="sa",password="changeme";
+        StrictMode.ThreadPolicy a =new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(a);
+        String ConnectURL=null;
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectURL="jdbc:jtds:sqlserver://"+ip+":"+port+";"+
+                    "databasename="+db+";user="+username+";"+"password="+password+";";
+            con= DriverManager.getConnection(ConnectURL);
+        } catch (Exception e) {
+            Log.e("Error is ",e.getMessage());
+        }
+        return con;
+
+    }
+}
