ba7e55d9aa20494d51bf217ddf29a8ea06784816
irthayagraj <gayathri.rp03@gmail.com>
2022-12-22 06:29:14 +0530

    trial

--- app/src/main/java/businesslayer/ConSql.java
+++ app/src/main/java/businesslayer/ConSql.java
@@ -11,7 +11,7 @@ public class ConSql {
     Connection con;
     @SuppressLint("NewApi")
     public Connection conClass(){
-        String ip="192.168.127.15",port="1433",db="librarymanagement",username="sa",password="changeme";
+
         StrictMode.ThreadPolicy a =new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(a);
         String ConnectURL=null;
