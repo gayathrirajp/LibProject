8bf51712f436af6623aa44932cc125f56131778a
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2022-12-27 11:50:35 +0530

    Changed app icon

--- app/src/main/java/businesslayer/ConSql.java
+++ app/src/main/java/businesslayer/ConSql.java
@@ -13,7 +13,7 @@ public class ConSql {
     Connection con;
     @SuppressLint("NewApi")
     public Connection conClass() {
-        String ip="192.168.112.36",port="1433",db="seeshelvesdb",username="sa",password="a1236700";
+        String ip="192.168.60.36",port="1433",db="seeshelvesdb",username="sa",password="a1236700";
         StrictMode.ThreadPolicy a =new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(a);
         String ConnectURL=null;
